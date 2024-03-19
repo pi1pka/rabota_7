6444d1992e96cdf220319c18e35c2b3c6c5ce2f2
pi1pka <163390775+pi1pka@users.noreply.github.com>
2024-03-19 12:41:20 +0300

    Update ConsoleApplication1.cpp

--- Задание 3/ConsoleApplication1.cpp
+++ Задание 3/ConsoleApplication1.cpp
@@ -8,12 +8,12 @@ int main()
 	int sum = 0;
 	int pr = 1;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
 		sum = sum + i;
 		pr = pr * i;
 	}
-	cout << "Сумма чисел от 1 до 100 = " << sum << endl;
-	cout << "Сумма чисел от 1 до 100 = " << pr << endl;
+	cout << "Сумма чисел от 1 до 99 = " << sum << endl;
+	cout << "Сумма чисел от 1 до 99 = " << pr << endl;
 	return 0;
 }
