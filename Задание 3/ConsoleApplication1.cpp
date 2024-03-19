bfd082cc90847f7da77637551d1f4b34f6afe45b
pi1pka <163390775+pi1pka@users.noreply.github.com>
2024-03-19 12:40:16 +0300

    Add files via upload

---
+++ Задание 3/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int i;
+	int sum = 0;
+	int pr = 1;
+
+	for (i = 1; i <= 100; i++)
+	{
+		sum = sum + i;
+		pr = pr * i;
+	}
+	cout << "Сумма чисел от 1 до 100 = " << sum << endl;
+	cout << "Сумма чисел от 1 до 100 = " << pr << endl;
+	return 0;
+}
---
+++ Задание 3/ап.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#include <time.h>
+int main() {
+time_t start,end;
+time(&start);
+time(&end);
+double seconds = difftime(end, start);
+printf("The time: %f seconds\n", seconds);
+}
